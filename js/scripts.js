8e4437b635fc24008cc31a6ce15d2db9739969c3
orkhan123 <orkhan.aghayev@outlook.com>
2024-03-15 19:09:01 +0400

    Update scripts.js
    Ex 1 data deleted and Ex 2 data added:
    pokemonList variable created,
    3 different pokemon with name, height, and type data added.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello World');
-
-let favoriteFood= 'Pasta';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Pikachu', height: 0.4, types:'Electric'},
+    {name: 'Seel', height: 1.1, types: 'Water'},
+    {name: 'Swalot', height: 1.7, types: 'Poison'}
+];
