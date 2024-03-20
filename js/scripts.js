5a8633dda08581d9444f5bb070bed94593f821d9
orkhan123 <orkhan.aghayev@outlook.com>
2024-03-20 20:05:10 +0400

    For loops on Pokemon list
    Using "for loops" for calling pokemonList table based on their height.

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,15 @@ let pokemonList = [
     {name: 'Pikachu', height: 0.4, types:'Electric'},
     {name: 'Seel', height: 1.1, types: 'Water'},
     {name: 'Swalot', height: 1.7, types: 'Poison'}
-];
+]
+
+for (let i=0; i < pokemonList.length; i++) 
+    if (pokemonList[i].height < 0.5)
+    {
+    document.write(pokemonList[i].name + "   " + "height:" + pokemonList[i].height + "   " + "This is small pokemon");
+    }
+
+    else if (pokemonList[i].height > 0.5)
+    {
+    document.write(pokemonList[i].name + "   " + "height:" + pokemonList[i].height);
+    }
