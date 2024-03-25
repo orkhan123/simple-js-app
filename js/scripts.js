a727029b3972f17333e5df1775284b4436d8c25d
orkhan123 <orkhan.aghayev@outlook.com>
2024-03-25 17:12:35 +0400

    Using forEach instead of For
    Using forEach making function of (pokemon) for pokemonList array.

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,11 @@ let pokemonList = [
     {name: 'Swalot', height: 1.7, types: 'Poison'}
 ]
 
-for (let i=0; i < pokemonList.length; i++) 
-    if (pokemonList[i].height < 0.5)
-    {
-    document.write(pokemonList[i].name + "   " + "height:" + pokemonList[i].height + "   " + "This is small pokemon");
-    }
-
-    else if (pokemonList[i].height > 0.5)
-    {
-    document.write(pokemonList[i].name + "   " + "height:" + pokemonList[i].height);
-    }
+pokemonList.forEach(function(pokemon) {
+    if (pokemon.height < 0.5)
+    document.write(pokemon.name + " " + "height:" + pokemon.height + " " + "This is small pokemon" + "<p>");
+    else if (1.5 > pokemon.height > 0.5)
+    document.write(pokemon.name + " " + "height:" + pokemon.height + " " + "This is medium pokemon" + "<p>");
+    else if (pokemon.height > 1.5)
+    document.write(pokemon.name + " " + "height:" + pokemon.height + " " + "This is big pokemon" + "<p>");
+});
