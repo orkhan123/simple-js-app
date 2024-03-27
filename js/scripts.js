96f9e1775fc22db43fd80fbd705d920b043eab6b
orkhan123 <orkhan.aghayev@outlook.com>
2024-03-27 13:46:49 +0400

    pokemonRepository
    creating a pokemonRepository for the pokemon list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,25 @@
-let pokemonList = [
+let pokemonRepository = (function () {
+  let pokemonList = [
     {name: 'Pikachu', height: 0.4, types:'Electric'},
     {name: 'Seel', height: 1.1, types: 'Water'},
-    {name: 'Swalot', height: 1.7, types: 'Poison'}
-]
+    {name: 'Swalot', height: 1.7, types: 'Poison'}];
 
-pokemonList.forEach(function(pokemon) {
-    if (pokemon.height < 0.5)
-    document.write(pokemon.name + " " + "height:" + pokemon.height + " " + "This is small pokemon" + "<p>");
-    else if (1.5 > pokemon.height > 0.5)
-    document.write(pokemon.name + " " + "height:" + pokemon.height + " " + "This is medium pokemon" + "<p>");
-    else if (pokemon.height > 1.5)
-    document.write(pokemon.name + " " + "height:" + pokemon.height + " " + "This is big pokemon" + "<p>");
+  return {
+    add: function(pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function() {
+      return pokemonList;
+    }
+  };
+})();
+
+
+pokemonRepository.getAll(function() {
+    if (pokemonRepository.height < 0.5)
+    document.write (pokemonRepository.name + " " + "height:" + pokemonRepository.height + " " + "This is small pokemon" + "<p>");
+    else if (1.5 > pokemonRepository.height > 0.5)
+    document.write (pokemonRepository.name + " " + "height:" + pokemonRepository.height + " " + "This is medium pokemon" + "<p>");
+    else if (pokemonRepository.height > 1.5)
+    document.write (pokemonRepository.name + " " + "height:" + pokemonRepository.height + " " + "This is big pokemon" + "<p>");
 });
