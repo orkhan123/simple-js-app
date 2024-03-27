d2848959e347135b54e6870115b938d72a64d8af
orkhan123 <orkhan.aghayev@outlook.com>
2024-03-27 14:29:22 +0400

    pokemonRepository update

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,36 @@
 let pokemonRepository = (function () {
-  let pokemonList = [
-    {name: 'Pikachu', height: 0.4, types:'Electric'},
-    {name: 'Seel', height: 1.1, types: 'Water'},
-    {name: 'Swalot', height: 1.7, types: 'Poison'}];
-
-  return {
-    add: function(pokemon) {
-      pokemonList.push(pokemon);
-    },
-    getAll: function() {
+    let pokemonList = [
+      { name: "Pikachu", height: 0.4, types: "Electric" },
+      { name: "Seel", height: 1.1, types: "Water" },
+      { name: "Swalot", height: 1.7, types: "Poison" },
+    ];
+  
+    function add(newItem) {
+      pokemonList.push(newItem);
+    }
+  
+    function getAll() {
       return pokemonList;
     }
-  };
-})();
-
-
-pokemonRepository.getAll(function() {
-    if (pokemonRepository.height < 0.5)
-    document.write (pokemonRepository.name + " " + "height:" + pokemonRepository.height + " " + "This is small pokemon" + "<p>");
-    else if (1.5 > pokemonRepository.height > 0.5)
-    document.write (pokemonRepository.name + " " + "height:" + pokemonRepository.height + " " + "This is medium pokemon" + "<p>");
-    else if (pokemonRepository.height > 1.5)
-    document.write (pokemonRepository.name + " " + "height:" + pokemonRepository.height + " " + "This is big pokemon" + "<p>");
-});
+  
+    function printPokemonList() {
+      pokemonList.forEach(function (pokemon) {
+        
+        if (pokemon.height < 0.5) {
+        document.write (pokemon.name + ", height: " + pokemon.height + " "+ "This is a small pokemon" + "<p>");
+        } else if (pokemon.height > 0.5 && pokemon.height <= 1.5) {
+        document.write (pokemon.name + ", height: " + pokemon.height + " "+ "This is a small pokemon" + "<p>");
+        } else {
+        document.write (pokemon.name + ", height: " + pokemon.height + " "+ "This is a small pokemon" + "<p>");
+        }
+      });
+    }
+  
+    return {
+      add: add,
+      getAll: getAll,
+      printPokemonList: printPokemonList,
+    };
+  })();
+  
+  pokemonRepository.printPokemonList();
